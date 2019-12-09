ba0e267ad489f5d9aae03fdee8c60677ce4fb8db
root <root@ip-172-31-46-238.ec2.internal>
2019-12-09 18:09:55 +0000

    initial commit

---
+++ packages.sh
@@ -0,0 +1,20 @@
+    8  sudo apt-get install libapr1-dev libaprutil1-dev
+   10  apt-get install pcre-devel 
+   11  sudo apt-get install pcre-devel 
+   12   sudo apt-get install libpcre3-dev
+   13  sudo apt-get install build-essential
+   18  apt-get install apr-util
+   19  sudo apt-get install apr-util
+   20  sudo apt-get install apr
+   21  sudo apt-get install libtool
+   37  apt-get install apache2-utils
+   38  sudo apt-get install apache2-utils
+   41  sudo apt-get install apr apr-util
+   42  sudo apt-get install apr-util
+   43  sudo apt-get install libapr1 libapr1-dev
+   52  sudo apt-get install libtoolT
+   54  sudo apt-get install libtoolT
+   99  apt-get install apt-file
+  100  sudo apt-get install apt-file
+  106  sudo apt-get install libbind-dev
+  107  sudo apt-get install apache2-dev
