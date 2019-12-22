e2f611dbcd1373e0946708a7b8e9ac51924ca17c
root <root@ip-172-31-26-224.ec2.internal>
2019-12-22 08:44:04 +0000

    added the docker installation script

---
+++ dokr_i_A.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y	
+
+# sudo yes
+
+sudo  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+apt-cache madison docker-ce
+
+sudo apt-get install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io
+
+sudo docker run hello-world
+
+
+
+####to uninstall
+#apt-get remove docker-ce
